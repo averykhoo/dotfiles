9d84cfa76e768dec2aa446aed514f09adda53a94
averykhoo <averykhoo@gmail.com>
2020-05-08 19:03:03 +0800

    https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
+# create symlinks from `~` to desired items in `~/dotfiles`
 
-ln -s ~/.bashrc ./.bashrc
+# old dotfiles backup directory
+old_dir=~/dotfile/bak
+mkdir -p ${old_dir}
 
-ln -s ~/.curlrc ./.curlrc
-ln -s ~/.wgetrc ./.wgetrc
+# dotfiles directory
+dotfiles_dir=~/dotfiles
 
-ln -s ~/.nano   ./.nano
-ln -s ~/.nanorc ./.nanorc
+# list of files/folders to symlink in homedir
+filenames=".bashrc .curlrc .wgetrc .nano .nanorc"
+
+# change to the dotfiles directory
+cd ${dotfiles_dir}
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in ${filenames}; do
+    mv ~/${file} ${old_dir}/${file}
+    ln -s ${dotfiles_dir}/${file} ~/${file}
+done
