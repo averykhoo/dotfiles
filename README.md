651356c5fb4d9bf6aa647f1d40fc9a24ea56fc3b
averykhoo <averykhoo@gmail.com>
2020-05-13 11:39:35 +0800

    add stuff to install

--- README.md
+++ README.md
@@ -2,7 +2,102 @@
 dotfiles
 
 
-#   Notes
+#   install
+*   [ ] aria2
+*   [ ] asciinema
+*   [ ] baobab?
+*   [ ] [bat](https://github.com/sharkdp/bat/releases/latest)
+*   [ ] browsh?
+*   [ ] byobu?
+*   [ ] chrome
+*   [ ] cifs
+*   [ ] curl
+*   [ ] emacs
+*   [ ] exa
+*   [ ] exfat-fuse
+*   [ ] fd (sharkdp)
+*   [ ] feh (CLI image viewer)
+*   [ ] ffmpeg (for JJ's speech)
+*   [ ] firefox
+*   [ ] fonts-noto-*
+*   [ ] fzf
+*   [ ] gedit + fonts-noto-color-emoji
+*   [ ] git & git-lfs
+*   [ ] gnome
+*   [ ] gparted
+*   [ ] gufw?
+*   [ ] hexyl? (Sharkdp) (hex viewer)
+*   [ ] htop / Glances (top-like dashboard, install via pip, web server needs bottle)
+*   [ ] httpie
+*   [ ] ifconfig (net-tools)
+*   [ ] jq
+*   [ ] micro?
+*   [ ] midnight Commander?
+*   [ ] mosh?
+*   [ ] mtr (better traceroute)
+*   [ ] nano
+*   [ ] ncdu
+*   [ ] nfs-server-*
+*   [ ] nmap & zenmap / zmap & zdns / masscan
+*   [ ] nmtui
+*   [ ] ntfs-3g (Ntfsprogs)
+*   [ ] oracle-java8 & java-altrenatives
+*   [ ] pandoc
+*   [ ] p7zip-*
+*   [ ] peco
+*   [ ] powerline
+*   [ ] powershell
+*   [ ] prettyping
+*   [ ] pydf
+*   [ ] ranger
+*   [ ] realpath? Alias pwd?
+*   [ ] ripgrep
+*   [ ] samba
+*   [ ] snlwflake (ssh client)
+*   [ ] socat
+*   [ ] sox (for JJ's speech)
+*   [ ] sshfs (sshfs-fuse)
+*   [ ] sshpass
+*   [ ] terminalizer
+*   [ ] tesseract (ocr)
+*   [ ] tldr (snap install tldr) (also update the data)
+*   [ ] tmux?
+*   [ ] trash-cli?
+*   [ ] tree (prefer exa tree?)
+*   [ ] ufw
+*   [ ] uidmap (for rootless docker)
+*   [ ] unzip
+*   [ ] vim
+*   [ ] vlc
+*   [ ] vncserver
+*   [ ] wordlist
+*   [ ] xclock
+*   [ ] xfce
+*   [ ] xsv?
+*   [ ] zcat
+*   [ ] zgrep
+*   [ ] zstd
+
+*   [ ] shellcheck
+*   [ ] locate, updatedb?
+*   [ ] hyperfine (sharkdp)
+*   [ ] pv
+*   [ ] unar lsar unp
+*   [ ] pglob pkill
+*   [ ] pigz
+*   [ ] xclip
+*   [ ] multitail
+*   [ ] httpie
+*   [ ] youtube-dl
+*   [ ] lnav
+*   [ ] autojump
+*   [ ] [ad](https://github.com/tanrax/terminal-AdvancedNewFile)
+*   [ ] [bat-extras](https://github.com/eth-p/bat-extras/releases/latest)
+*   [ ] [delta](https://github.com/dandavison/delta/releases/latest)
+    *   look for `delta-*-x86_64-unknown-linux-musl.tar.gz`
+
+
+#   notes
 *   nanorc
     *   from [scopatz/nanorc](https://github.com/scopatz/nanorc)
     *   slightly modified (added all existing `*.nanorc` files, some were missing in provided nanorc)
---
+++ install.sh
@@ -0,0 +1,270 @@
+#!/usr/bin/env bash
+
+
+
+echo "Installing aria2"
+#apt install -y aria2
+
+echo "Installing asciinema"
+apt-add-repository ppa:zanchey/asciinema
+apt-get update
+apt install -y asciinema
+
+echo "Installing baobab"
+apt install -y baobab
+
+echo "Installing bat"
+#apt install -y bat
+
+echo "Installing browsh?"
+#apt install -y browsh?
+
+echo "Installing byobu?"
+#apt install -y byobu?
+
+echo "Installing chrome"
+#apt install -y chrome
+
+echo "Installing cifs"
+#apt install -y cifs
+
+echo "Installing curl"
+#apt install -y curl
+
+echo "Installing emacs"
+#apt install -y emacs
+
+echo "Installing exa"
+#apt install -y exa
+
+echo "Installing exfat-fuse"
+#apt install -y exfat-fuse
+
+echo "Installing fd (sharkdp)"
+#apt install -y fd (sharkdp)
+
+echo "Installing feh (CLI image viewer)"
+#apt install -y feh (CLI image viewer)
+
+echo "Installing ffmpeg"
+#apt install -y ffmpeg
+
+echo "Installing firefox"
+#apt install -y firefox
+
+echo "Installing fonts-noto-*"
+#apt install -y fonts-noto-*
+
+echo "Installing fzf"
+#apt install -y fzf
+
+echo "Installing gedit + fonts-noto-color-emoji"
+#apt install -y gedit + fonts-noto-color-emoji
+
+echo "Installing git & git-lfs"
+#apt install -y git & git-lfs
+
+echo "Installing gnome"
+#apt install -y gnome
+
+echo "Installing gparted"
+#apt install -y gparted
+
+echo "Installing gufw?"
+#apt install -y gufw?
+
+echo "Installing hexyl? (Sharkdp) (hex viewer)"
+#apt install -y hexyl? (Sharkdp) (hex viewer)
+
+echo "Installing htop / Glances (top-like dashboard, install via pip, web server needs bottle)"
+#apt install -y htop / Glances (top-like dashboard, install via pip, web server needs bottle)
+
+echo "Installing httpie"
+#apt install -y httpie
+
+echo "Installing ifconfig (net-tools)"
+#apt install -y ifconfig (net-tools)
+
+echo "Installing jq"
+#apt install -y jq
+
+echo "Installing micro?"
+#apt install -y micro?
+
+echo "Installing midnight Commander?"
+#apt install -y midnight Commander?
+
+echo "Installing mosh?"
+#apt install -y mosh?
+
+echo "Installing mtr (better traceroute)"
+#apt install -y mtr (better traceroute)
+
+echo "Installing nano"
+#apt install -y nano
+
+echo "Installing ncdu"
+#apt install -y ncdu
+
+echo "Installing nfs-server-*"
+#apt install -y nfs-server-*
+
+echo "Installing nmap & zenmap / zmap & zdns / masscan"
+#apt install -y nmap & zenmap / zmap & zdns / masscan
+
+echo "Installing nmtui"
+#apt install -y nmtui
+
+echo "Installing ntfs-3g (Ntfsprogs)"
+#apt install -y ntfs-3g (Ntfsprogs)
+
+echo "Installing oracle-java8 & java-altrenatives"
+#apt install -y oracle-java8 & java-altrenatives
+
+echo "Installing pandoc"
+#apt install -y pandoc
+
+echo "Installing p7zip-*"
+#apt install -y p7zip-*
+
+echo "Installing peco"
+#apt install -y peco
+
+echo "Installing powerline"
+#apt install -y powerline
+
+echo "Installing powershell"
+#apt install -y powershell
+
+echo "Installing prettyping"
+#apt install -y prettyping
+
+echo "Installing pydf"
+#apt install -y pydf
+
+echo "Installing ranger"
+#apt install -y ranger
+
+echo "Installing realpath? Alias pwd?"
+#apt install -y realpath? Alias pwd?
+
+echo "Installing ripgrep"
+#apt install -y ripgrep
+
+echo "Installing samba"
+#apt install -y samba
+
+echo "Installing snlwflake (ssh client)"
+#apt install -y snlwflake (ssh client)
+
+echo "Installing socat"
+#apt install -y socat
+
+echo "Installing sox (for JJ's speech)"
+#apt install -y sox (for JJ's speech)
+
+echo "Installing sshfs (sshfs-fuse)"
+#apt install -y sshfs (sshfs-fuse)
+
+echo "Installing sshpass"
+#apt install -y sshpass
+
+echo "Installing terminalizer"
+#apt install -y terminalizer
+
+echo "Installing tesseract (ocr)"
+#apt install -y tesseract (ocr)
+
+echo "Installing tldr (snap install tldr) (also update the data)"
+#apt install -y tldr (snap install tldr) (also update the data)
+
+echo "Installing tmux?"
+#apt install -y tmux?
+
+echo "Installing trash-cli?"
+#apt install -y trash-cli?
+
+echo "Installing tree (prefer exa tree?)"
+#apt install -y tree (prefer exa tree?)
+
+echo "Installing ufw"
+#apt install -y ufw
+
+echo "Installing uidmap (for rootless docker)"
+#apt install -y uidmap (for rootless docker)
+
+echo "Installing unzip"
+#apt install -y unzip
+
+echo "Installing vim"
+#apt install -y vim
+
+echo "Installing vlc"
+#apt install -y vlc
+
+echo "Installing vncserver"
+#apt install -y vncserver
+
+echo "Installing wordlist"
+#apt install -y wordlist
+
+echo "Installing xclock"
+#apt install -y xclock
+
+echo "Installing xfce"
+#apt install -y xfce
+
+echo "Installing xsv?"
+#apt install -y xsv?
+
+echo "Installing zcat"
+#apt install -y zcat
+
+echo "Installing zgrep"
+#apt install -y zgrep
+
+echo "Installing zstd"
+#apt install -y zstd
+
+
+echo "Installing shellcheck"
+#apt install -y shellcheck
+
+echo "Installing locate, updatedb?"
+#apt install -y locate, updatedb?
+
+echo "Installing hyperfine (sharkdp)"
+#apt install -y hyperfine (sharkdp)
+
+echo "Installing pv"
+#apt install -y pv
+
+echo "Installing unar lsar unp"
+#apt install -y unar lsar unp
+
+echo "Installing pglob pkill"
+#apt install -y pglob pkill
+
+echo "Installing pigz"
+#apt install -y pigz
+
+echo "Installing xclip"
+#apt install -y xclip
+
+echo "Installing multitail"
+#apt install -y multitail
+
+echo "Installing httpie"
+#apt install -y httpie
+
+echo "Installing youtube-dl"
+#apt install -y youtube-dl
+
+echo "Installing lnav"
+#apt install -y lnav
+
+echo "Installing autojump"
+#apt install -y autojump
+
+echo "Installing ad"
+pip3 install --user advance-touch
