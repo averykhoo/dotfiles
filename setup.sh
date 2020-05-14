fcf5dfb3ba1a1f32557dfd456b0dd3a43c84d57c
averykhoo <averykhoo@gmail.com>
2020-05-14 20:06:09 +0800

    uppercase vars for consistency

--- setup.sh
+++ setup.sh
@@ -2,29 +2,31 @@
 # create symlinks from `~` to desired items in `~/dotfiles`
 
 # dotfiles directory
-dotfiles_dir=~/dotfiles
-if [[ -d ${dotfiles_dir} ]]; then
+DOTFILES_DIR=~/dotfiles
+if [[ -d ${DOTFILES_DIR} ]]; then
     # old dotfiles backup directory
-    backup_dir=${dotfiles_dir}/backup--$(date +"%Y-%m-%d--%H-%M-%S")
+    backup_dir=${DOTFILES_DIR}/backup--$(date +"%Y-%m-%d--%H-%M-%S")
     mkdir -p ${backup_dir}
 else
-    git clone --depth 1 https://github.com/averykhoo/dotfiles.git dotfiles_dir
+    git clone --depth 1 https://github.com/averykhoo/dotfiles.git DOTFILES_DIR
 fi
 
 # list of files/folders to symlink in homedir
-filenames=".bashrc .curlrc .wgetrc .nano .nanorc"
+FILENAMES=".bashrc .curlrc .wgetrc .nano .nanorc"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in ${filenames}
+for file in ${FILENAMES}
 do
-    if [[ ! -f "${dotfiles_dir}/${file}" ]]
-    then
-        echo "${dotfiles_dir}/${file} does not exist"
+    if [[ ! -f "${DOTFILES_DIR}/${file}" ]]; then
+        echo "${DOTFILES_DIR}/${file} does not exist"
 
-    elif cmp --silent "~/${file}" "${dotfiles_dir}/${file}"
-    then
+    elif cmp --silent "~/${file}" "${DOTFILES_DIR}/${file}"; then
         mv "~/${file}" "${backup_dir}/${file}"
-        ln -s "${dotfiles_dir}/${file}" "~/${file}"
+        ln -s "${DOTFILES_DIR}/${file}" "~/${file}"
 
     fi
 done
+
+unset FILENAMES
+
+unset DOTFILES_DIR
