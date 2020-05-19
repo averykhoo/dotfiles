9296abd792c73d3b150ed6e8e729c0bb39b77344
averykhoo <averykhoo@gmail.com>
2020-05-19 16:37:36 +0800

    update chrome path

--- install-rhel.sh
+++ install-rhel.sh
@@ -222,9 +222,9 @@ fi
 
 echo "Installing chrome"
 if [[ ! -x "$(command -v google-chrome)" ]]; then
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.rpm
-    sudo yum install -y google-chrome-stable_current_amd64.rpm
-    rm google-chrome-stable_current_amd64.rpm
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+    sudo yum install -y google-chrome-stable_current_x86_64.rpm
+    rm google-chrome-stable_current_x86_64.rpm
 else
     echo "already installed"
 fi
