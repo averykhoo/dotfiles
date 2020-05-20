fe2ac93e0d74a1a02c45b0042bf7c606fa85498b
Avery Khoo <averykhoo@gmail.com>
2020-05-20 14:41:24 +0800

    make exec

--- rhel-dvd-repo.sh
+++ rhel-dvd-repo.sh
@@ -3,17 +3,17 @@
 ISO_FILE_BASENAME="rhel-8.2-x86_64-dvd"
 ISO_SHA256_CHECKSUM=7fdfed9c7cced4e526a362e64ed06bcdc6ce0394a98625a40e7d05db29bf7b86
 
-MOUNT_PATH="/mnt/${ISO_FILE_BASENAME}"
-ISO_FILE_NAME="${ISO_FILE_BASENAME}.iso"
-REPO_FILE_NAME="${ISO_FILE_BASENAME}.repo"
+# remove spaces
+ISO_FILE_SAFENAME=${ISO_FILE_BASENAME// /-}
+ISO_MOUNT_PATH="/mnt/${ISO_FILE_SAFENAME}"
 
 # we need sudo permissions
 sudo echo "setting up repo using RHEL dvd"
 
 # create mount point
 echo ""
-echo "step 1/7: create mount point at [${MOUNT_PATH}]"
-sudo mkdir --parents "${MOUNT_PATH}"
+echo "step 1/7: create mount point at [${ISO_MOUNT_PATH}]"
+sudo mkdir --parents "${ISO_MOUNT_PATH}"
 if [[ $? != 0 ]]; then
     echo "ERROR: unable to create mount point"
     exit
@@ -24,7 +24,7 @@ echo "step 2/7: mount RHEL installation media"
 blkid /dev/sr0
 if [[ $? == 0 ]]; then
     echo "physical dvd found"
-    sudo mount /dev/sr0 "${MOUNT_PATH}"
+    sudo mount /dev/sr0 "${ISO_MOUNT_PATH}"
     if [[ $? != 0 ]]; then
         echo "ERROR: dvd mount failed"
         exit
@@ -32,14 +32,14 @@ if [[ $? == 0 ]]; then
 
 else
     echo "physical dvd not inserted, verifying iso file"
-    echo "${ISO_SHA256_CHECKSUM} ${ISO_FILE_NAME}" | sha256sum --check -
+    echo "${ISO_SHA256_CHECKSUM} ${ISO_FILE_SAFENAME}.iso" | sha256sum --check -
     if [[ $? != 0 ]]; then
         echo "ERROR: iso checksum verification failed"
         exit
     fi
 
     echo "mounting iso file"
-    sudo mount -o loop "${ISO_FILE_NAME}" "${MOUNT_PATH}"
+    sudo mount -o loop "${ISO_FILE_SAFENAME}.iso" "${ISO_MOUNT_PATH}"
     if [[ $? != 0 ]]; then
         echo "ERROR: iso mount failed"
         exit
@@ -48,7 +48,7 @@ fi
 
 # copy data
 echo "step 3/7: copy data"
-sudo cp -R "${MOUNT_PATH}" /home
+sudo cp -R "${ISO_MOUNT_PATH}" /home
 if [[ $? != 0 ]]; then
     echo "ERROR: copy failed"
     exit
@@ -56,7 +56,7 @@ fi
 
 # unmount
 echo "step 4/7: unmount RHEL installation media"
-sudo umount "${MOUNT_PATH}"
+sudo umount "${ISO_MOUNT_PATH}"
 if [[ $? != 0 ]]; then
     echo "ERROR: unmount failed"
     exit
@@ -64,20 +64,20 @@ fi
 
 # setup repo
 echo "step 5/7: copy repo file"
-sudo cp /home/${ISO_FILE_BASENAME}/media.repo "/etc/yum.repos.d/${REPO_FILE_NAME}"
+sudo cp /home/${ISO_FILE_SAFENAME}/media.repo "/etc/yum.repos.d/${ISO_FILE_SAFENAME}.repo"
 if [[ $? != 0 ]]; then
     echo "ERROR: repo file copy failed"
     exit
 fi
-echo "enabled=1" | sudo tee -a "/etc/yum.repos.d/${REPO_FILE_NAME}"
-echo "baseurl=file:///home/${ISO_FILE_BASENAME}" | sudo tee -a "/etc/yum.repos.d/${REPO_FILE_NAME}"
+echo "enabled=1" | sudo tee -a "/etc/yum.repos.d/${ISO_FILE_SAFENAME}.repo"
+echo "baseurl=file:///home/${ISO_FILE_SAFENAME}" | sudo tee -a "/etc/yum.repos.d/${ISO_FILE_SAFENAME}.repo"
 if [[ $? != 0 ]]; then
     echo "ERROR: updating repo file failed"
     exit
 fi
 
 echo "step 6/7: chmod repo file"
-sudo chmod 644 "/etc/yum.repos.d/${REPO_FILE_NAME}"
+sudo chmod 644 "/etc/yum.repos.d/${ISO_FILE_SAFENAME}.repo"
 if [[ $? != 0 ]]; then
     echo "ERROR: repo file chmod failed"
     exit
