ceb62d5c9d7ce31c7e31591cd34e5746caea7bbd
Avery Khoo <averykhoo@gmail.com>
2022-12-19 11:22:26 +0800

    add example to backup script, and add a maybe todo

--- vendored/backup.sh
+++ vendored/backup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# todo: consider a flag for making the backup read-only
+
 if [[ $# -eq 1 ]]; then
     cp -pvi "$1" "${1}.copied--$(date +'%Y-%m-%d--%H-%M-%S').bak"
 else
-    echo "Info:  $0 copies to a backup file"
-    echo "Usage: $0 <file to be backed up with .bak extension>"
+    echo "Info:     $0 clones a target file to a backup copy"
+    echo "Usage:    $0 <file to be backed up with .bak extension>"
+    echo "Example: '$0 important-file.txt' would create a backup called important-file.txt.copied--2022-02-22--20:22:02.bak"
 fi
